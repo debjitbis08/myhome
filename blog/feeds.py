9f2e9da18c9cb5dc3a9f858ffd4a500ebfe37753
Debjit Biswas <debjitbis08@gmail.com>
2011-04-12 00:04:23 +0530

    Fix code indentation (replace tabs with spaces)

--- blog/feeds.py
+++ blog/feeds.py
@@ -2,15 +2,15 @@ from django.contrib.syndication.views import Feed
 from myhome.blog.models import Entry 
 
 class LatestEntriesFeed(Feed):
-	title = "Debjit Biswas's Weblog"
-	link = "/"
-	description = "Debjit Biswas's Weblog"
+    title = "Debjit Biswas's Weblog"
+    link = "/"
+    description = "Debjit Biswas's Weblog"
 
-	def items(self):
-		return Entry.objects.order_by('-pub_date')[:10]
+    def items(self):
+        return Entry.objects.order_by('-pub_date')[:10]
 
-	def item_title(self, item):
-		return item.title
+    def item_title(self, item):
+        return item.title
 
-	def item_description(self, item):
-		return item.body_text
+    def item_description(self, item):
+        return item.body_text
