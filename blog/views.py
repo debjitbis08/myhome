7b43fcb67dfbee06c3e36726ad4285a4570f0dff
Debjit Biswas <debjitbis08@gmail.com>
2011-04-12 00:04:51 +0530

    Fix code indentation (replace tabs with spaces)

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,11 @@ from django.views.generic import list_detail
 from django.shortcuts import get_object_or_404
 from myhome.blog.models import Entry
 
-def entry_list(request,design='False', num=0):
-	return list_detail.object_list(
-		request,
-		queryset = Entry.objects.all().order_by('-pub_date'),
-		paginate_by = 5,
-		page = num,
-	)
+def entry_list(request, num=0):
+    return list_detail.object_list(
+        request,
+        queryset = Entry.objects.all().order_by('-pub_date'),
+        paginate_by = 5,
+        page = num,
+    )
 
