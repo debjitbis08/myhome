d81c1e923c69504179666c04133f38c904d96e7c
Debjit Biswas <debjitbis08@gmail.com>
2011-04-12 00:03:49 +0530

    Fix code indentation (replace tabs with spaces)

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,8 @@ from myhome.blog.models import Author
 from django.contrib import admin
 
 class EntryAdmin(admin.ModelAdmin):
-	fields = ('blog', 'title', 'slug', 'body_text', 'author','pub_date')
-	prepopulated_fields = {"slug" : ('title',)}
+    fields = ('blog', 'title', 'slug', 'body_text', 'author','pub_date')
+    prepopulated_fields = {"slug" : ('title',)}
 
 admin.site.register(Blog)
 admin.site.register(Author)
