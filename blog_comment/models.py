d85de12dca4f91e8a6d753ed34bfaf41b9bb742a
Debjit Biswas <debjitbis08@gmail.com>
2011-04-22 16:38:31 -0500

    Fix blog_comment app

--- blog_comment/models.py
+++ blog_comment/models.py
@@ -1,5 +1,5 @@
 from django.contrib.comments.models import Comment
 from django.contrib.comments.signals import comment_was_posted
-from blog_comment.signals import comment_notifier
+from myhome.blog_comment.signals import comment_notifier
 
 comment_was_posted.connect(comment_notifier, sender=Comment)
