d797d9efdd73a2832bc2cb6fb51d6b87f0eeaad5
Debjit Biswas <debjitbis08@gmail.com>
2011-08-09 23:24:49 +0530

    Remove gallery link.

--- static/assets/js/base.js
+++ static/assets/js/base.js
@@ -1,15 +1,5 @@
 $(function() {
 
-  $(".galleryLink").overlay({
-    expose: {
-      color: '#333333',
-      loadSpeed: 100,
-      opacity: 0.9
-    },
-
-    closeOnClick: false
-  });
-
   //Load Markdown help section
   if ($('#commentForm').length > 0){
     $('#markdownhelp').find('.contentWrap').load('/media/assets/static/markdownhelp.html #dingus');
